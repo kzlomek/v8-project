87015a45fdb612983bb2a1cf3e6d62c2dd79d739
Krystian Zlomek <k.zlomek@eyeo.com>
2019-02-04 21:20:54 +0100

    Test previous build.
    Build version 7.0.
    
    Signed-off-by: Krystian Zlomek <k.zlomek@eyeo.com>

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ import subprocess
 import sys
 
 depot_tools_repository_url = 'https://chromium.googlesource.com/chromium/tools/depot_tools.git'
-default_v8_revision = '4d72a9931a125d21901d25d67896f0e40105bd16'
+default_v8_revision = '5f246c123ea463b189ca0579c0a9074459e20a6f'
 
 this_dir_path = os.path.dirname(os.path.realpath(__file__))
 third_party = 'third_party'
